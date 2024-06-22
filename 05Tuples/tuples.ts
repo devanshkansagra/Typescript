9ebcaec5c0b7b3a1da09bc6a18754a3367cb30ec
devanshkansagra <dkansagra04@gmail.com>
2024-06-22 19:07:28 +0530

    Tuples

---
+++ 05Tuples/tuples.ts
@@ -0,0 +1,5 @@
+const User: readonly [number, string, number, boolean] = [1, 'Devansh Kansagra', 7433070704, true];
+console.log(User);
+console.log(typeof(User));
+// User.push("dkansagra03@gmail.com"); // Gives error
+console.log(User);
